333d1e842cbfaed71995b2b3e45292e3fbaf3fee
Vasyaruberoid <84591840+Vasyaruberoid@users.noreply.github.com>
2024-09-03 22:42:47 +0300

    fixxxx

--- hws2-master/src/s2-homeworks/hw01/message/Message.tsx
+++ hws2-master/src/s2-homeworks/hw01/message/Message.tsx
@@ -6,23 +6,23 @@ import { MessagePropsType } from "../HW1";
 const Message = ({ message }: { message: MessagePropsType }) => {
   return (
     <div id={"hw1-message-" + message.id} className={s.message}>
-      <div className={s.imageAndText}>
-        <div className={s.text}>
-          <div id={"hw1 -name-" + message.id} className={s.name}>
-            {message.user.name}
-          </div>
-          <pre id={"hw1-text-" + message.id} className={s.messageText}>
-            {message.message.text}
-          </pre>
-        </div>
-        <div className={s.block}>
-          <img id={"hw1-avatar-" + message.id} src={message.user.avatar} alt='avatar' width='50' height='50'/>
-          <div id={"hw1-time-" + message.id} className={s.time}>
-            {message.message.time}
-          </div>
+    <div className={s.imageAndText}>
+      <div className={s.block}>
+        <img id={"hw1-avatar-" + message.id} src={message.user.avatar} alt='avatar' width='50' height='50'/>
+        <div id={"hw1-time-" + message.id} className={s.time}>
+          {message.message.time}
         </div>
       </div>
+      <div className={s.text}>
+        <div id={"hw1-name-" + message.id} className={s.name}>
+          {message.user.name}
+        </div>
+        <pre id={"hw1-text-" + message.id} className={s.messageText}>
+          {message.message.text}
+        </pre>
+      </div>
     </div>
+  </div>
   );
 };
 
